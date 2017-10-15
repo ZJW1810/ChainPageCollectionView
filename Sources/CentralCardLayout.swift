40db9e94c8f2b74062af0c045cdd0efb0b8234fc
ZJW1810 <shadowy1810@gmail.com>
2017-10-15 11:54:36 -0400

    This is a test.

--- Sources/CentralCardLayout.swift
+++ Sources/CentralCardLayout.swift
@@ -30,6 +30,7 @@
 
 import UIKit
 
+// Add some notes: maxScaleOffset controls the distance of maximum scale.
 private let maxScaleOffset: CGFloat = 200
 private let minScale: CGFloat = 0.9
 private let minAlpha: CGFloat = 0.3
